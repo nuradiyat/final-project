1785be1bb8ea37b4a64d1a18d6c3230ef8490c9d
Adiffa19 <adiffafitriana625@gmail.com>
2024-07-04 19:47:38 +0700

    add chef and menu

---
+++ Onado Cafe/menu.php
@@ -0,0 +1,110 @@
+    <!-- ======= Menu Section ======= -->
+    <section id="menu" class="menu">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Check our tasty <span>Menu</span></h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="menu-flters">
+              <li data-filter="*" class="filter-active">Show All</li>
+              <li data-filter=".filter-Drinks">Drinks</li>
+              <li data-filter=".filter-Foods">Foods</li>
+              <li data-filter=".filter-Snacks">Snacks</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row menu-container">
+
+          <div class="col-lg-6 menu-item filter-Foods">
+            <div class="menu-content">
+              <a href="#">Nasi ayam goreng sambal korek </a><span>28K</span>
+            </div>
+            <div class="menu-ingredients">
+              nasi, ayam goreng, sambal korek
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Snacks">
+            <div class="menu-content">
+              <a href="#">French Fries</a><span>20K</span>
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Foods">
+            <div class="menu-content">
+              <a href="#">Nasi ayam sambal matah</a><span>30K</span>
+            </div>
+            <div class="menu-ingredients">
+            nasi, ayam fillet tepung, sambal matah,
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Snacks">
+            <div class="menu-content">
+              <a href="#">Onado platter</a><span>26K</span>
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Foods">
+            <div class="menu-content">
+              <a href="#">Nasi ayam teriyaki</a><span>30K</span>
+            </div>
+            <div class="menu-ingredients">
+              Nasi,ayam fillet tepung,teriyaki sauce
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Snacks">
+            <div class="menu-content">
+              <a href="#">Mozarella stick</a><span>20K</span>
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Foods">
+            <div class="menu-content">
+              <a href="#">Nasi kulit ayam</a><span>28K</span>
+            </div>
+            <div class="menu-ingredients">
+            nasi,kulit ayam crispy,sambal matah
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Snacks">
+            <div class="menu-content">
+              <a href="#">Roti Bakar coklat keju </a><span>20K</span>
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Drinks">
+            <div class="menu-content">
+              <a href="#">Kopi susu onado</a><span>23K</span>
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Drinks">
+            <div class="menu-content">
+              <a href="#">Americano </a><span>20K</span>
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Drinks">
+            <div class="menu-content">
+              <a href="#">Ice tea</a><span>12K</span>
+            </div>
+          </div>
+
+          <div class="col-lg-6 menu-item filter-Drinks">
+            <div class="menu-content">
+              <a href="#">Fruity Bom</a><span>26K</span>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+<!-- End Menu Section -->
